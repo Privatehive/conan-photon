713fa3e99a52cb9514608195e860b74367c8278c
Björn Stresing <bjoern.stresing@gmx.de>
2025-03-08 00:13:43 +0100

    Switch to Photon stable release. Add patched Photon release with ADM support

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ from conan import ConanFile
 from conan.tools.files import copy, replace_in_file
 from conan.tools.files import get
 from conan.tools.env import Environment
+from conan.tools.env import Environment
 from conan.errors import ConanInvalidConfiguration
 import json, os, io
 
@@ -17,7 +18,7 @@ class Photon(ConanFile):
     name = jsonInfo["projectName"]
     version = jsonInfo["version"]
     user = jsonInfo["domain"]
-    channel = "snapshot"
+    channel = "stable"
     # ---Metadata---
     description = jsonInfo["projectDescription"]
     license = jsonInfo["license"]
@@ -27,7 +28,7 @@ class Photon(ConanFile):
     url = jsonInfo["repository"]
     # ---Requirements---
     requires = []
-    tool_requires = ["openjdk/[~19]@%s/stable" % user]
+    tool_requires = ["openjdk/[~8]@%s/stable" % user]
     # ---Sources---
     exports = ["info.json"]
     exports_sources = []
@@ -44,44 +45,61 @@ class Photon(ConanFile):
         valid_os = ["Windows", "Linux", "Macos"]
         if str(self.settings.os) not in valid_os:
             raise ConanInvalidConfiguration(f"{self.name} {self.version} is only supported for the following operating systems: {valid_os}")
+        valid_arch = ["x86_64", "armv8"]
+        if str(self.settings.arch) not in valid_arch:
+            raise ConanInvalidConfiguration(f"{self.name} {self.version} is only supported for the following architectures on {self.settings.os}: {valid_arch}")
 
     def source(self):
-        get(self, "https://github.com/fschleich/photon/archive/aef41c3afe78ffe425afe2b56444ef664df294a4.zip", strip_root=True)
-        version = self.version
-        if self.channel == "snapshot":
-            version += "-snapshot"
-        replace_in_file(self, os.path.join(self.source_folder, "build.gradle"), "JavaLanguageVersion.of(11)", "JavaLanguageVersion.of(19)")
-        replace_in_file(self, os.path.join(self.source_folder, "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "\"0.0.0\"", "\"%s\"" % version)
-        replace_in_file(self, os.path.join(self.source_folder, "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "return theClass.getPackage().getImplementationVersion();", "return \"%s\";" % version)
+        get(self, "https://github.com/Netflix/photon/archive/refs/tags/v4.10.8.zip", destination="photon", strip_root=True)
+        #replace_in_file(self, os.path.join(self.source_folder, "build.gradle"), "JavaLanguageVersion.of(11)", "JavaLanguageVersion.of(19)")
+        replace_in_file(self, os.path.join(self.source_folder, "photon", "build.gradle"), "id 'nebula.netflixoss' version '11.1.1'", "")
+        replace_in_file(self, os.path.join(self.source_folder, "photon", "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "\"0.0.0\"", "\"%s\"" % self.version)
+        replace_in_file(self, os.path.join(self.source_folder, "photon", "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "return theClass.getPackage().getImplementationVersion();", "return \"%s\";" % self.version)
+        get(self, "https://github.com/wruppelx/photon/archive/refs/heads/feature/ADMAudio.zip", destination="photon-adm", strip_root=True)
+        #replace_in_file(self, os.path.join(self.source_folder, "build.gradle"), "JavaLanguageVersion.of(11)", "JavaLanguageVersion.of(19)")
+        replace_in_file(self, os.path.join(self.source_folder, "photon-adm", "build.gradle"), "id 'nebula.netflixoss' version '11.1.1'", "")
+        replace_in_file(self, os.path.join(self.source_folder, "photon-adm", "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "\"0.0.0\"", "\"%s-adm\"" % self.version)
+        replace_in_file(self, os.path.join(self.source_folder, "photon-adm", "src", "main", "java", "com", "netflix", "imflibrary", "utils", "Utilities.java"), "return theClass.getPackage().getImplementationVersion();", "return \"%s\";" % self.version)
 
-    def build(self):
 
-        with open(os.path.join(self.build_folder, "gradle.properties"), "w") as f:
+    def build_photon(self, buildFolder: str):
+        
+        with open(os.path.join(buildFolder, "gradle.properties"), "w") as f:
             f.write('version=%s' % self.version)
 
         env = Environment()
-        env.define("GRADLE_USER_HOME", os.path.join(self.build_folder, "gradle_home"))
+        env.define("GRADLE_USER_HOME", os.path.join(buildFolder, "gradle_home"))
         env.define("GRADLE_OPTS", "-Dorg.gradle.daemon=false")
         envvars = env.vars(self)
         with envvars.apply():
             stdout = io.StringIO()
             if self.settings.os == "Macos" or self.settings.os == "Linux":
-                self.run("chmod +x gradlew")
-                self.run("./gradlew build -x test")
-                self.run("./gradlew getDependencies")
-                self.run("jdeps --multi-release 19 --module-path \"$JAVA_HOME/jmods\" -cp 'build/libs/*' --print-module-deps build/libs/Photon-%s.jar" % self.version, stdout=stdout)
-                self.run("jlink --compress 2 --strip-debug --no-header-files --no-man-pages --module-path \"$JAVA_HOME/jmods\" --output jre --add-modules java.desktop,%s" % stdout.getvalue())
+                self.run("chmod +x gradlew", cwd=buildFolder)
+                self.run("./gradlew build -x test", cwd=buildFolder)
+                self.run("./gradlew getDependencies", cwd=buildFolder)
+                #self.run("jdeps --multi-release 19 --module-path \"$JAVA_HOME/jmods\" -cp 'build/libs/*' --print-module-deps build/libs/Photon-%s.jar" % self.version, stdout=stdout)
+                #self.run("jlink --compress 2 --strip-debug --no-header-files --no-man-pages --module-path \"$JAVA_HOME/jmods\" --output jre --add-modules java.desktop,%s" % stdout.getvalue())
             else:
-                self.run("gradlew.bat build -x test")
-                self.run("gradlew.bat getDependencies")
-                self.run("jdeps --multi-release 19 --module-path \"%%JAVA_HOME%%/jmods\" -cp build/libs/* --print-module-deps build/libs/Photon-%s.jar" % self.version, stdout=stdout)
-                self.run("jlink --compress 2 --strip-debug --no-header-files --no-man-pages --module-path \"%%JAVA_HOME%%/jmods\" --output jre --add-modules java.desktop,%s" % stdout.getvalue())
+                self.run("gradlew.bat build -x test", cwd=buildFolder)
+                self.run("gradlew.bat getDependencies", cwd=buildFolder)
+                #self.run("jdeps --multi-release 19 --module-path \"%%JAVA_HOME%%/jmods\" -cp build/libs/* --print-module-deps build/libs/Photon-%s.jar" % self.version, stdout=stdout)
+                #self.run("jlink --compress 2 --strip-debug --no-header-files --no-man-pages --module-path \"%%JAVA_HOME%%/jmods\" --output jre --add-modules java.desktop,%s" % stdout.getvalue())
+
+
+    def build(self):
+        self.build_photon(os.path.join(self.build_folder, "photon"))
+        self.build_photon(os.path.join(self.build_folder, "photon-adm"))
+        if self.settings.os == "Macos" or self.settings.os == "Linux":
+            self.run("cp -r \"$JAVA_HOME/jre\" \"%s\"" % self.build_folder)
+        else:
+            self.run("xcopy \"%%JAVA_HOME%%\\jre\" \"%s\" /E /H /C /I" % self.build_folder)
 
     def package(self):
-        copy(self, pattern="*.jar", src=os.path.join(self.build_folder, "build", "libs"), dst=os.path.join(self.package_folder, "photon"))
+        copy(self, pattern="*.jar", src=os.path.join(self.build_folder, "photon", "build", "libs"), dst=os.path.join(self.package_folder, "photon"))
+        copy(self, pattern="*.jar", src=os.path.join(self.build_folder, "photon-adm", "build", "libs"), dst=os.path.join(self.package_folder, "photon-adm"))
         copy(self, pattern="*", src=os.path.join(self.build_folder, "jre"), dst=os.path.join(self.package_folder, "jre"))
         with open(os.path.join(self.package_folder, "Findphoton.cmake"), "w") as f:
-            f.write('file(GLOB photon_jar_files "${CMAKE_CURRENT_LIST_DIR}/photon/*.jar")\nset(photon_files ${photon_jar_files} CACHE STRING "")\nset(photon_dir "${CMAKE_CURRENT_LIST_DIR}/photon" CACHE STRING "")\nset(photon_jre_dir "${CMAKE_CURRENT_LIST_DIR}/jre" CACHE STRING "")')
+            f.write('set(photon_dir "${CMAKE_CURRENT_LIST_DIR}/photon" CACHE STRING "")\nset(photon_adm_dir "${CMAKE_CURRENT_LIST_DIR}/photon-adm" CACHE STRING "")\nset(photon_jre_dir "${CMAKE_CURRENT_LIST_DIR}/jre" CACHE STRING "")')
 
     def package_info(self):
         self.cpp_info.set_property("cmake_find_mode", "none")
